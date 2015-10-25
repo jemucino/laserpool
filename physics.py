a59d872a1cde0798faf51e9d2234b37d5ef5911b
Eduardo Mucino <jemucino@gmail.com>
2015-10-24 19:19:08 -0700

    physics module can determine if collision occurred

--- physics.py
+++ physics.py
@@ -16,6 +16,7 @@ class Ball:
 #     self.v = s[3]
 
     self.mass = .17
+    self.radius = .026
 
   def move(self, s, t):
     ds = [0,0,0,0]
@@ -42,7 +43,7 @@ class Table:
   def __init__(self, num_balls):
     '''Define the state of the pool table'''
     self.balls = range(num_balls)
-    self.next_ball = 1
+    self.next_ball = 0
 
     self.length = 2.235
     self.width = 1.143
@@ -57,15 +58,24 @@ class Table:
     self.next_ball += 1
     return {'number': number, 'state': state}
 
+  def detect_collision(self):
+    for i, first_ball in enumerate(self.balls):
+      for second_ball in self.balls[i+1:]:
+        distance = math.sqrt((first_ball.s[0]-second_ball.s[0])**2 + (first_ball.s[1]-second_ball.s[1])**2)
+        if distance < 2*first_ball.radius:
+          print 'collision: ', first_ball.number, second_ball.number
+
   def propagate_state(self, timestep = 1e-1):
-    for i in range(10):
+    for i in range(100):
       for ball in self.balls:
           ball.propagate_state(timestep)
           print ball.number, ': ', ball.s
+
+      self.detect_collision()
 
 
 if __name__ == '__main__':
   from scipy.integrate import odeint
 
-  table = Table(2)
+  table = Table(16)
   table.propagate_state()
